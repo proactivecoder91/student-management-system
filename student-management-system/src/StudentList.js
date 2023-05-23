4ca84638f174b34bda92d5f2c2a61617df49a79a
chivalrous <tony@starkindustries>
2023-05-23 15:23:20 +0530

    feat: Called the get api to display the list of students on the UI using the useEffect and useState hooks

--- student-management-system/src/StudentList.js
+++ student-management-system/src/StudentList.js
@@ -1,7 +1,49 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
-function studentList() {
-  return <div>studentList</div>;
-}
+const StudentList = () => {
+  const [student, setStudent] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:8000/students")
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((data) => {
+        setStudent(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+  return (
+    <>
+      <div className="card">
+        <div className="card-title">
+          <h2>Student List</h2>
+        </div>
+        <div className="card-body">
+          <table className="table table-bordered table-striped">
+            <thead className="bg-dark text-white">
+              <tr>
+                <td>ID</td>
+                <td>Name</td>
+                <td>Age</td>
+              </tr>
+            </thead>
+            <tbody>
+              {student &&
+                student.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.age}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default studentList;
+export default StudentList;
